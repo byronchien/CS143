0a75cc4122757695defa379f0b48f7b19ad198f8
byronchien <byronc@ucla.edu>
2016-10-02 22:13:56 -0700

    byron: eval stuff for calculator

--- vm-shared/calculator.php
+++ vm-shared/calculator.php
@@ -16,9 +16,15 @@
 	{
 		$calc_input = $_POST["calc_input"];
 		$matches = [];
-		preg_match("/([0-9]+[+-\/*])+([0-9]+)/",$calc_input, $matches);
-		var_dump($matches);
-		echo preg_last_error();
+/* code for checking for valid expressions?
+ /		preg_match("/([0-9]+[+-\/*])+([0-9]+)/",$calc_input, $matches);
+ /		var_dump($matches);
+ /		echo preg_last_error();
+ */
+
+/* code for evaluating valid expression */
+		$result = eval("return $calc_input;");
+		echo $result;
 	}
 
  ?>
