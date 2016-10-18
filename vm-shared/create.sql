dabeb8690c6554786f6b8bccaf5eef3f2ce696b2
Nathaniel Chu <nathanielchu4@gmail.com>
2016-10-18 15:49:08 -0700

    Added more tables

--- vm-shared/create.sql
+++ vm-shared/create.sql
@@ -15,6 +15,9 @@ CREATE TABLE Actor(
 	dob DATE,
 	dod DATE,
 	PRIMARY KEY (id),
+	CHECK (last IS NOT NULL AND last <> ''),
+	CHECK (first IS NOT NULL AND first <> ''),
+	CHECK (sex IS NOT NULL AND sex <> ''),
 	CHECK (dob IS NOT NULL));
 
 CREATE TABLE Director(
@@ -24,4 +27,35 @@ CREATE TABLE Director(
 	dob DATE,
 	dod DATE,
 	PRIMARY KEY (id),
+	CHECK (last IS NOT NULL AND last <> ''),
+	CHECK (first IS NOT NULL AND first <> ''),
 	CHECK (dob IS NOT NULL));
+
+CREATE TABLE MovieGenre(
+	mid INT,
+	genre VARCHAR(20),
+	FOREIGN KEY	(mid) REFERENCES Movie(id),
+	CHECK (genre IS NOT NULL AND genre <> ''),
+	);
+
+CREATE TABLE MovieDirector(
+	mid INT,
+	did INT,
+	PRIMARY KEY (did),
+	FOREIGN KEY	(mid) REFERENCES Movie(id));
+
+CREATE TABLE MovieActor(
+	mid INT,
+	aid INT,
+	role VARCHAR(50),
+	FOREIGN KEY (mid) REFERENCES Movie(id),
+	FOREIGN KEY (aid) REFERENCES Actor(id),
+	CHECK (role IS NOT NULL AND role <> ''));
+
+CREATE TABLE Review(
+	name VARCHAR(20),
+	time TIMESTAMP,
+	mid INT,
+	rating INT,
+	comment VARCHAR(500),
+	FOREIGN KEY (mid) REFERENCES Movie(id));
